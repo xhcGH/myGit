b7a9910e338ebbc6fe5b534d5cfec997ad4b45a2
xhcGH <xhc14517@163.com>
2018-04-21 16:09:40 +0800

    Update index.php

--- lendoo-web/public/index.php
+++ lendoo-web/public/index.php
@@ -114,7 +114,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = './lendoo-web/public';
+	$application_folder = '../application';
 
 /*
  *---------------------------------------------------------------
